9c5b7a638f22173995c4aff9653d8a4db1f9bad1
Aflian <137276645+Aflian@users.noreply.github.com>
2023-08-13 21:26:57 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,123 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" href="style.css">
+	<script src="https://kit.fontawesome.com/de8ae7ab0d.js" crossorigin="anonymous"></script>
+	<title>CERITA TAQIYA</title>
+</head>
+
+<body>
+	<!-- navigasi -->
+	<nav>
+		<img src="./assets/cerita_taqiya-removebg-preview.png" alt="#">
+		<ul>
+			<li> <i class="fa-solid fa-house"></i><a href="#profile">HOME</a></li>
+			<li> <i class="fa-solid fa-book"></i><a href="novel.php">STORY</a></li>
+			<li> <i class="fa-solid fa-link"></i><a href="#social">SOCIAL</a></li>
+			<li> <i class="fa-solid fa-user"></i><a href="/pages/login.html">LOGIN</a></li>
+		</ul>
+	</nav>
+	<!-- content -->
+	<section id="profile">
+		<div class="container">
+			<div class="taqi-content">
+				<div class="taqi-img">
+					<img src="./assets/taqi-banner.png" alt="">
+				</div>
+				<div class="taqi-desc">
+					<h2>HI <span>SALMA TAQIYA HERE 😊</span></h2>
+					<p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nostrum animi aut provident ad atque eveniet
+						doloribus libero ipsum molestias iure est dolor, esse tenetur, reiciendis recusandae commodi
+						necessitatibus! Doloremque, rem.x</p>
+					<a href="/">MY COLLECTION</a>
+				</div>
+			</div>
+		</div>
+	</section>
+	<section id="story">
+		<div class="container-1">
+			<h2>MY COLLETION</h2>
+			<div class="taqi-card">
+				<div class="card">
+					<img class="img-card" src="./assets/library.jpg" alt="">
+					<h3 class="title-card">
+						NOVEL 1
+					</h3>
+					<p class="card-desc">
+						Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam maiores modi molestiae ratione.
+						Tempore soluta ratione officiis, odio voluptates perspiciatis libero quibusdam consequatur impedit
+						illum.
+					</p>
+					<a href="">READ HERE </a>
+				</div>
+				<div class="card">
+					<img class="img-card" src="./assets/library.jpg" alt="">
+					<h3 class="title-card">
+						NOVEL 1
+					</h3>
+					<p class="card-desc">
+						Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam maiores modi molestiae ratione.
+						Tempore soluta ratione officiis, odio voluptates perspiciatis libero quibusdam consequatur impedit
+						illum.
+					</p>
+					<a href="">READ HERE </a>
+				</div>
+				<div class="card">
+					<img class="img-card" src="./assets/library.jpg" alt="">
+					<h3 class="title-card">
+						NOVEL 1
+					</h3>
+					<p class="card-desc">
+						Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam maiores modi molestiae ratione.
+						Tempore soluta ratione officiis, odio voluptates perspiciatis libero quibusdam consequatur impedit
+						illum.
+					</p>
+					<a href="">READ HERE </a>
+				</div>
+			</div>
+			<a href="" id="link-book">MORE NOVEL...</a>
+		</div>
+	</section>
+	<!-- FOOTER -->
+	<footer class="footer">
+		<section id="social">
+			<div class="container-foot">
+				<h1 id="banner-footer" >CERITA TAQIYA</h1>
+				<div class="row">
+					<div class="footer-col">
+						<h4>company</h4>
+						<ul>
+							<li><a href="#">about me</a></li>
+							<li><a href="#">our services</a></li>
+							<li><a href="#">privacy policy</a></li>
+						</ul>
+					</div>
+					<div class="footer-col">
+						<h4>online shop</h4>
+						<ul>
+							<li><a href="#">bag</a></li>
+							<li><a href="#">shoes</a></li>
+							<li><a href="#">dress</a></li>
+						</ul>
+					</div>
+					<div class="footer-col">
+						<h4>follow us</h4>
+						<div class="social-links">
+							<a href="#"><i class="fab fa-facebook-f"></i></a>
+							<a href="#"><i class="fab fa-twitter"></i></a>
+							<a href="#"><i class="fab fa-instagram"></i></a>
+							<a href="#"><i class="fab fa-linkedin-in"></i></a>
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
+	</footer>
+
+
+</body>
+
+</html>
